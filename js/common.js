70b8a3a36773d9f4095c3a2255e0f5bc4b70e581
Sergey Matiyasevich <FoxRandom@Foxs-MacBook-Pro.local>
2014-10-22 21:27:17 +0400

    v0.1

--- js/common.js
+++ js/common.js
@@ -1,33 +1,28 @@
-$( '#feed' ).css( 'display', 'block' );
-
-var activePan = {
-	settings : function() {
-		$( '.right_col' ).css( 'width', '60px' );
-		$( '.left_col' ).css( 'width', '300px' );
-		$( '.center_col' ).css( 'padding-left', '300px' );
-		$( '.center_col' ).css( 'padding-right', '60px' );
-		$( '.left_cont' ).delay( 400 ).show( 0 );
-		$( '.right_cont' ).hide();
-	},
-	garments : function() {
-		$( '.right_col' ).css( 'width', '300px' );
-		$( '.left_col' ).css( 'width', '60px' );
-		$( '.center_col' ).css( 'padding-left', '60px' );
-		$( '.center_col' ).css( 'padding-right', '300px' );
-		$( '.right_cont' ).delay( 400 ).show( 0 );
-		$( '.left_cont' ).hide();
-	}
+function scrollTo(target){
+        var speed = 1000;
+        var top = $('#' + target).offset().top;
+        $('html, body').animate({scrollTop: top}, speed);
+        return false;
 }
 
-$( '.col_btn' ).click(function () {
-	  $( '.col_btn' ).css( 'color', 'rgba(256, 256, 256, 0.6)' );
-	  $( '.col_btn' ).css( 'background-color', 'rgba(0, 0, 0, 0)' );
-      $( this ).css( 'color', '#292929' );
-	  $( this ).css( 'background-color', 'rgba(256, 256, 256, 0.6)' );
-	  $( '.col_content_inner' ).css( 'display', 'none' );
-	  var btnAct = $( this ).attr( 'title' );
-	  $( '#' + btnAct ).css( 'display', 'block' );
-	  var clrTheme = $( '#' + btnAct ).attr( 'data-clr' );
-	  $( '.left_col' ).css( 'background-color', clrTheme );
-	  $( '.right_col' ).css( 'background-color', clrTheme );
-    });
+function init() {
+	var initClr = $( '.btn_init_state' ).attr( 'data-pclr' );
+	$( '.btn_init_state' ).css( 'background-color', initClr );
+	$( '.btn_init_state' ).css( 'color', '#fff' );
+	scrollTo( 'p1' );
+}
+
+$( '.nav_btn' ).click(function() {
+	
+	  $( '.nav_btn' ).css( 'color', '#000' );
+	  $( '.nav_btn' ).css( 'background-color', 'rgba(0, 0, 0, 0)' );
+	
+	var scrollTarget = $( this ).attr( 'data-target' );
+	var btnClr = $( this ).attr( 'data-pclr' );
+	
+      $( this ).css( 'color', '#fff' );
+	  $( this ).css( 'background-color', btnClr );
+	
+	scrollTo( scrollTarget );
+
+});
