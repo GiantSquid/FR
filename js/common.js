598a967533895428db49ff76bafaaa649d2d894c
Sergey Matiyasevich <FoxRandom@Foxs-MacBook-Pro.local>
2014-10-22 23:03:10 +0400

    v0.2

--- js/common.js
+++ js/common.js
@@ -2,7 +2,6 @@ function scrollTo(target){
         var speed = 1000;
         var top = $('#' + target).offset().top;
         $('html, body').animate({scrollTop: top}, speed);
-        return false;
 }
 
 function init() {
@@ -14,7 +13,9 @@ function init() {
 
 $( '.nav_btn' ).click(function() {
 	
-	  $( '.nav_btn' ).css( 'color', '#000' );
+	$( 'body' ).off( 'waypoint', '.page' );
+	
+      $( '.nav_btn' ).css( 'color', '#000' );
 	  $( '.nav_btn' ).css( 'background-color', 'rgba(0, 0, 0, 0)' );
 	
 	var scrollTarget = $( this ).attr( 'data-target' );
@@ -24,5 +25,18 @@ $( '.nav_btn' ).click(function() {
 	  $( this ).css( 'background-color', btnClr );
 	
 	scrollTo( scrollTarget );
+	
+	$( 'body' ).on( 'waypoint', '.page' );
 
 });
+
+/*$( '.page' ).waypoint(function(direction) {
+	var vp = $( this ).attr('id');
+	$( '.nav_btn' ).css( 'color', '#000' );
+	$( '.nav_btn' ).css( 'background-color', 'rgba(0, 0, 0, 0)' );
+	
+	var curBtn = $( '.' + vp );
+	var curBtnClr = curBtn.attr( 'data-pclr' );
+	curBtn.css( 'background-color', curBtnClr );
+	curBtn.css( 'color', '#fff' );
+});*/
